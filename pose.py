6aa163eb9de26271ece3efa4a3b6a3f8a4609986
cskitty <melody.yue.yu@gmail.com>
2022-02-10 19:58:33 -0800

    pose python

---
+++ pose.py
@@ -0,0 +1,344 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import pandas as pd
+
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_pose = mp.solutions.pose
+
+
+cap = cv2.VideoCapture("/Volumes/MelodyOldSSD/melody/ai/tennis/melissa1/train1.mp4")
+size = (480,640)
+out = cv2.VideoWriter("/Volumes/MelodyOldSSD/melody/tennis/out_melissa1.avi",cv2.VideoWriter_fourcc('M','J','P','G'), 5, size)
+
+column_names = ["#"]
+for i in range(33):
+    column_names.append("x" + str(i))
+    column_names.append("y" + str(i))
+    column_names.append("z" + str(i))
+
+df = pd.DataFrame(columns = column_names)
+print(df.shape)
+class FullBodyPoseEmbedder(object):
+    """Converts 3D pose landmarks into 3D embedding."""
+
+    def __init__(self, torso_size_multiplier=2.5):
+        # Multiplier to apply to the torso to get minimal body size.
+        self._torso_size_multiplier = torso_size_multiplier
+
+        # Names of the landmarks as they appear in the prediction.
+        self._landmark_names = [
+            'nose',
+            'left_eye_inner', 'left_eye', 'left_eye_outer',
+            'right_eye_inner', 'right_eye', 'right_eye_outer',
+            'left_ear', 'right_ear',
+            'mouth_left', 'mouth_right',
+            'left_shoulder', 'right_shoulder',
+            'left_elbow', 'right_elbow',
+            'left_wrist', 'right_wrist',
+            'left_pinky_1', 'right_pinky_1',
+            'left_index_1', 'right_index_1',
+            'left_thumb_2', 'right_thumb_2',
+            'left_hip', 'right_hip',
+            'left_knee', 'right_knee',
+            'left_ankle', 'right_ankle',
+            'left_heel', 'right_heel',
+            'left_foot_index', 'right_foot_index',
+        ]
+
+    def __call__(self, landmarks):
+        """Normalizes pose landmarks and converts to embedding
+
+        Args:
+          landmarks - NumPy array with 3D landmarks of shape (N, 3).
+
+        Result:
+          Numpy array with pose embedding of shape (M, 3) where `M` is the number of
+          pairwise distances defined in `_get_pose_distance_embedding`.
+        """
+        assert landmarks.shape[0] == len(self._landmark_names), 'Unexpected number of landmarks: {}'.format(
+            landmarks.shape[0])
+
+        # Get pose landmarks.
+        landmarks = np.copy(landmarks)
+
+        # Normalize landmarks.
+        landmarks = self._normalize_pose_landmarks(landmarks)
+
+        # Get embedding.
+        embedding = self._get_pose_distance_embedding(landmarks)
+
+        return embedding
+
+    def _normalize_pose_landmarks(self, landmarks):
+        """Normalizes landmarks translation and scale."""
+        landmarks = np.copy(landmarks)
+
+        # Normalize translation.
+        pose_center = self._get_pose_center(landmarks)
+        landmarks -= pose_center
+
+        # Normalize scale.
+        pose_size = self._get_pose_size(landmarks, self._torso_size_multiplier)
+        landmarks /= pose_size
+        # Multiplication by 100 is not required, but makes it eaasier to debug.
+        landmarks *= 100
+
+        return landmarks
+
+    def _get_pose_center(self, landmarks):
+        """Calculates pose center as point between hips."""
+        left_hip = landmarks[self._landmark_names.index('left_hip')]
+        right_hip = landmarks[self._landmark_names.index('right_hip')]
+        center = (left_hip + right_hip) * 0.5
+        return center
+
+    def _get_pose_size(self, landmarks, torso_size_multiplier):
+        """Calculates pose size.
+
+        It is the maximum of two values:
+          * Torso size multiplied by `torso_size_multiplier`
+          * Maximum distance from pose center to any pose landmark
+        """
+        # This approach uses only 2D landmarks to compute pose size.
+        landmarks = landmarks[:, :2]
+
+        # Hips center.
+        left_hip = landmarks[self._landmark_names.index('left_hip')]
+        right_hip = landmarks[self._landmark_names.index('right_hip')]
+        hips = (left_hip + right_hip) * 0.5
+
+        # Shoulders center.
+        left_shoulder = landmarks[self._landmark_names.index('left_shoulder')]
+        right_shoulder = landmarks[self._landmark_names.index('right_shoulder')]
+        shoulders = (left_shoulder + right_shoulder) * 0.5
+
+        # Torso size as the minimum body size.
+        torso_size = np.linalg.norm(shoulders - hips)
+
+        # Max dist to pose center.
+        pose_center = self._get_pose_center(landmarks)
+        max_dist = np.max(np.linalg.norm(landmarks - pose_center, axis=1))
+
+        return max(torso_size * torso_size_multiplier, max_dist)
+
+    def _get_pose_distance_embedding(self, landmarks):
+        """Converts pose landmarks into 3D embedding.
+
+        We use several pairwise 3D distances to form pose embedding. All distances
+        include X and Y components with sign. We differnt types of pairs to cover
+        different pose classes. Feel free to remove some or add new.
+
+        Args:
+          landmarks - NumPy array with 3D landmarks of shape (N, 3).
+
+        Result:
+          Numpy array with pose embedding of shape (M, 3) where `M` is the number of
+          pairwise distances.
+        """
+        embedding = np.array([
+            # One joint.
+
+            self._get_distance(
+                self._get_average_by_names(landmarks, 'left_hip', 'right_hip'),
+                self._get_average_by_names(landmarks, 'left_shoulder', 'right_shoulder')),
+
+            self._get_distance_by_names(landmarks, 'left_shoulder', 'left_elbow'),
+            self._get_distance_by_names(landmarks, 'right_shoulder', 'right_elbow'),
+
+            self._get_distance_by_names(landmarks, 'left_elbow', 'left_wrist'),
+            self._get_distance_by_names(landmarks, 'right_elbow', 'right_wrist'),
+
+            self._get_distance_by_names(landmarks, 'left_hip', 'left_knee'),
+            self._get_distance_by_names(landmarks, 'right_hip', 'right_knee'),
+
+            self._get_distance_by_names(landmarks, 'left_knee', 'left_ankle'),
+            self._get_distance_by_names(landmarks, 'right_knee', 'right_ankle'),
+
+            # Two joints.
+
+            self._get_distance_by_names(landmarks, 'left_shoulder', 'left_wrist'),
+            self._get_distance_by_names(landmarks, 'right_shoulder', 'right_wrist'),
+
+            self._get_distance_by_names(landmarks, 'left_hip', 'left_ankle'),
+            self._get_distance_by_names(landmarks, 'right_hip', 'right_ankle'),
+
+            # Four joints.
+
+            self._get_distance_by_names(landmarks, 'left_hip', 'left_wrist'),
+            self._get_distance_by_names(landmarks, 'right_hip', 'right_wrist'),
+
+            # Five joints.
+
+            self._get_distance_by_names(landmarks, 'left_shoulder', 'left_ankle'),
+            self._get_distance_by_names(landmarks, 'right_shoulder', 'right_ankle'),
+
+            self._get_distance_by_names(landmarks, 'left_hip', 'left_wrist'),
+            self._get_distance_by_names(landmarks, 'right_hip', 'right_wrist'),
+
+            # Cross body.
+
+            self._get_distance_by_names(landmarks, 'left_elbow', 'right_elbow'),
+            self._get_distance_by_names(landmarks, 'left_knee', 'right_knee'),
+
+            self._get_distance_by_names(landmarks, 'left_wrist', 'right_wrist'),
+            self._get_distance_by_names(landmarks, 'left_ankle', 'right_ankle'),
+
+            # Body bent direction.
+
+            # self._get_distance(
+            #     self._get_average_by_names(landmarks, 'left_wrist', 'left_ankle'),
+            #     landmarks[self._landmark_names.index('left_hip')]),
+            # self._get_distance(
+            #     self._get_average_by_names(landmarks, 'right_wrist', 'right_ankle'),
+            #     landmarks[self._landmark_names.index('right_hip')]),
+        ])
+
+        return embedding
+
+    def _get_average_by_names(self, landmarks, name_from, name_to):
+        lmk_from = landmarks[self._landmark_names.index(name_from)]
+        lmk_to = landmarks[self._landmark_names.index(name_to)]
+        return (lmk_from + lmk_to) * 0.5
+
+    def _get_distance_by_names(self, landmarks, name_from, name_to):
+        lmk_from = landmarks[self._landmark_names.index(name_from)]
+        lmk_to = landmarks[self._landmark_names.index(name_to)]
+        return self._get_distance(lmk_from, lmk_to)
+
+    def _get_distance(self, lmk_from, lmk_to):
+        return lmk_to - lmk_from
+
+
+
+def calculate_angle(a, b, c):
+    a = np.array(a)  # First
+    b = np.array(b)  # Mid
+    c = np.array(c)  # End
+
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(radians * 180.0 / np.pi)
+
+    if angle > 180.0:
+        angle = 360 - angle
+
+    return round(angle,1)
+
+
+def plot_angels(p1,p2, p3):
+    # Get coordinates
+    shoulder = [landmarks[p1].x,
+                landmarks[p1].y]
+    elbow = [landmarks[p2].x, landmarks[p2].y]
+    wrist = [landmarks[p3].x, landmarks[p3].y]
+
+    # Calculate angle
+    angle = calculate_angle(shoulder, elbow, wrist)
+
+    # Visualize angle
+    cv2.putText(image, str(angle),
+                tuple(np.multiply(elbow, size).astype(int)),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                )
+
+
+
+
+
+counter = 0
+img_count = 0
+stage = None
+output_images = []
+
+with mp_pose.Pose(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as pose:
+  while cap.isOpened():
+    success, image = cap.read()
+
+    if not success:
+      print("Ignoring empty camera frame.")
+      # If loading a video, use 'break' instead of 'continue'.
+      break
+
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = pose.process(image)
+
+    # Draw the pose annotation on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    # Extract landmarks
+    try:
+        landmarks = results.pose_landmarks.landmark
+        """
+        #plot_landmarks(results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
+
+        plot_angels(mp_pose.PoseLandmark.LEFT_SHOULDER.value, mp_pose.PoseLandmark.LEFT_ELBOW.value, mp_pose.PoseLandmark.LEFT_WRIST.value)
+        plot_angels(mp_pose.PoseLandmark.RIGHT_SHOULDER.value, mp_pose.PoseLandmark.RIGHT_ELBOW.value, mp_pose.PoseLandmark.RIGHT_WRIST.value)
+
+        plot_angels(mp_pose.PoseLandmark.LEFT_HIP.value, mp_pose.PoseLandmark.LEFT_KNEE.value,
+                    mp_pose.PoseLandmark.LEFT_ANKLE.value)
+        plot_angels(mp_pose.PoseLandmark.RIGHT_HIP.value, mp_pose.PoseLandmark.RIGHT_KNEE.value,
+                mp_pose.PoseLandmark.RIGHT_ANKLE.value)
+
+        plot_angels(mp_pose.PoseLandmark.LEFT_SHOULDER.value, mp_pose.PoseLandmark.LEFT_HIP.value, mp_pose.PoseLandmark.LEFT_KNEE.value)
+        plot_angels(mp_pose.PoseLandmark.RIGHT_SHOULDER.value, mp_pose.PoseLandmark.RIGHT_HIP.value, mp_pose.PoseLandmark.RIGHT_KNEE.value)
+"""
+    except:
+        pass
+
+
+    # do not display hand, feet
+    img_count += 1
+    marks = [img_count]
+    for idx, landmark in enumerate(results.pose_world_landmarks.landmark):
+        marks.append(landmark.x)
+        marks.append(landmark.y)
+        marks.append(landmark.z)
+
+        if idx in [1,2,3,4,5,6,7,8,9,10,17,18,19,20,21,22,29,30,31,32]:
+            results.pose_world_landmarks.landmark[idx].visibility = 0
+
+    if img_count > 0:
+        mp_drawing.plot_landmarks(results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
+        # Render detections
+
+    for idx, landmark in enumerate(results.pose_landmarks.landmark):
+        if idx in [1,2,3,4,5,6,7,8,9,10,17,18,19,20,21,22,29,30,31,32]:
+            results.pose_landmarks.landmark[idx].visibility = 0
+
+
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=2, circle_radius=2),
+                                mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                             )
+
+    df.loc[img_count] = marks
+    out.write(image)
+
+
+    # Flip the image horizontally for a selfie-view display.
+    cv2.imshow('Tennis Pose', image)
+    outImageFile = '/Volumes/MelodyOldSSD/melody/ai/tennis/melissa'+ str(img_count) +'.jpg'
+    cv2.imwrite(outImageFile, image)
+
+    if cv2.waitKey(5) & 0xFF == 27:
+      break
+
+
+    #break
+print(df.shape)
+print(df.head())
+h5File = "coach1.h5";
+df.to_hdf(h5File, "/data/coach1")
+
+#output = np.stack(output_images, axis=0)
+#to_gif(output, fps=10)
+cap.release()
+out.release()
